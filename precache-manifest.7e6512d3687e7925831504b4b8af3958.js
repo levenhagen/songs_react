a7cf915a3939a98ef9023fb8d21ac77fd0ed30d9
Bernardo <levenhal@gmail.com>
2019-01-15 01:18:30 -0200

    Updates

---
+++ precache-manifest.7e6512d3687e7925831504b4b8af3958.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b15166b54ea80c8a50f",
+    "url": "/songs_react/static/js/runtime~main.9b15166b.js"
+  },
+  {
+    "revision": "bdcf32525aef58deb55b",
+    "url": "/songs_react/static/js/main.bdcf3252.chunk.js"
+  },
+  {
+    "revision": "362085d2b739ffae3b4d",
+    "url": "/songs_react/static/js/1.362085d2.chunk.js"
+  },
+  {
+    "revision": "564e2f94594304928a79c4bf7ce2dc08",
+    "url": "/songs_react/index.html"
+  }
+];
